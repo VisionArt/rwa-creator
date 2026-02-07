c8b7ff26b9a41df0e2a51c27c2bcb826ab973a96
Karen Lee <karenkk.lee@protonmail.com>
2026-02-07 11:29:45 +0800

    Rename contract from sTSLA to sXAU and update logic to reflect the backed asset

--- src/sXAU.sol
+++ src/sXAU.sol
@@ -5,23 +5,23 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {OracleLib, AggregatorV3Interface} from "./libraries/OracleLib.sol";
 
 /*
- * @dev the codebase will mint sTSLA based on the collateral 
+ * @dev the codebase will mint sXAU based on the collateral 
  * deposited into this contract. In our example, ETH is the
- * collateral that we will use to mint sTSLA.
+ * collateral that we will use to mint sXAU.
  * 
  * This codebase is NOT COMPLETE
  * 
  * As far as the incentives to do this, people who want to 
  * short tesla and long eth would have the incentive to do this. 
  */
-contract sTSLA is ERC20 {
+contract sXAU is ERC20 {
     using OracleLib for AggregatorV3Interface;
 
-    error sTSLA_feeds__InsufficientCollateral();
+    error sXAU_feeds__InsufficientCollateral();
 
     // These both have 8 decimal places for Polygon
     // https://docs.chain.link/data-feeds/price-feeds/addresses?network=polygon
-    address private i_tslaFeed;
+    address private i_xauFeed;
     address private i_ethUsdFeed;
     uint256 public constant DECIMALS = 8;
     uint256 public constant ADDITIONAL_FEED_PRECISION = 1e10;
@@ -31,24 +31,24 @@ contract sTSLA is ERC20 {
     uint256 private constant LIQUIDATION_PRECISION = 100;
     uint256 private constant MIN_HEALTH_FACTOR = 1e18;
 
-    mapping(address user => uint256 tslaMinted) public s_tslaMintedPerUser;
+    mapping(address user => uint256 xauMinted) public s_xauMintedPerUser;
     mapping(address user => uint256 ethCollateral) public s_ethCollateralPerUser;
 
-    constructor(address tslaFeed, address ethUsdFeed) ERC20("Synthetic Tesla (Feeds)", "sTSLA") {
-        i_tslaFeed = tslaFeed;
+    constructor(address xauFeed, address ethUsdFeed) ERC20("Synthetic Tesla (Feeds)", "sXAU") {
+        i_xauFeed = xauFeed;
         i_ethUsdFeed = ethUsdFeed;
     }
 
     /* 
-     * @dev User must deposit at least 200% of the value of the sTSLA they want to mint
+     * @dev User must deposit at least 200% of the value of the sXAU they want to mint
      */
     function depositAndmint(uint256 amountToMint) external payable {
         // Checks / Effects
         s_ethCollateralPerUser[msg.sender] += msg.value;
-        s_tslaMintedPerUser[msg.sender] += amountToMint;
+        s_xauMintedPerUser[msg.sender] += amountToMint;
         uint256 healthFactor = getHealthFactor(msg.sender);
         if (healthFactor < MIN_HEALTH_FACTOR) {
-            revert sTSLA_feeds__InsufficientCollateral();
+            revert sXAU_feeds__InsufficientCollateral();
         }
         _mint(msg.sender, amountToMint);
         // No external interactions
@@ -56,19 +56,19 @@ contract sTSLA is ERC20 {
 
     function redeemAndBurn(uint256 amountToRedeem) external {
         // Checks / Effects
-        uint256 valueRedeemed = getUsdAmountFromTsla(amountToRedeem);
+        uint256 valueRedeemed = getUsdAmountFromXau(amountToRedeem);
         uint256 ethToReturn = getEthAmountFromUsd(valueRedeemed);
-        s_tslaMintedPerUser[msg.sender] -= amountToRedeem;
+        s_xauMintedPerUser[msg.sender] -= amountToRedeem;
         s_ethCollateralPerUser[msg.sender] -= ethToReturn;
         uint256 healthFactor = getHealthFactor(msg.sender);
         if (healthFactor < MIN_HEALTH_FACTOR) {
-            revert sTSLA_feeds__InsufficientCollateral();
+            revert sXAU_feeds__InsufficientCollateral();
         }
         _burn(msg.sender, amountToRedeem);
         // External
         (bool success,) = msg.sender.call{value: ethToReturn}("");
         if (!success) {
-            revert("sTSLA_feeds: transfer failed");
+            revert("sXAU_feeds: transfer failed");
         }
     }
 
@@ -76,14 +76,14 @@ contract sTSLA is ERC20 {
                              VIEW AND PURE
     //////////////////////////////////////////////////////////////*/
     function getHealthFactor(address user) public view returns (uint256) {
-        (uint256 totalTslaMintedValueInUsd, uint256 totalCollateralEthValueInUsd) = getAccountInformationValue(user);
-        return _calculateHealthFactor(totalTslaMintedValueInUsd, totalCollateralEthValueInUsd);
+        (uint256 totalXauMintedValueInUsd, uint256 totalCollateralEthValueInUsd) = getAccountInformationValue(user);
+        return _calculateHealthFactor(totalXauMintedValueInUsd, totalCollateralEthValueInUsd);
     }
 
-    function getUsdAmountFromTsla(uint256 amountTslaInWei) public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(i_tslaFeed);
+    function getUsdAmountFromXau(uint256 amountXauInWei) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(i_xauFeed);
         (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
-        return (amountTslaInWei * (uint256(price) * ADDITIONAL_FEED_PRECISION)) / PRECISION;
+        return (amountXauInWei * (uint256(price) * ADDITIONAL_FEED_PRECISION)) / PRECISION;
     }
 
     function getUsdAmountFromEth(uint256 ethAmountInWei) public view returns (uint256) {
@@ -101,29 +101,29 @@ contract sTSLA is ERC20 {
     function getAccountInformationValue(address user)
         public
         view
-        returns (uint256 totalTslaMintedValueUsd, uint256 totalCollateralValueUsd)
+        returns (uint256 totalXauMintedValueUsd, uint256 totalCollateralValueUsd)
     {
-        (uint256 totalTslaMinted, uint256 totalCollateralEth) = _getAccountInformation(user);
-        totalTslaMintedValueUsd = getUsdAmountFromTsla(totalTslaMinted);
+        (uint256 totalXauMinted, uint256 totalCollateralEth) = _getAccountInformation(user);
+        totalXauMintedValueUsd = getUsdAmountFromXau(totalXauMinted);
         totalCollateralValueUsd = getUsdAmountFromEth(totalCollateralEth);
     }
 
-    function _calculateHealthFactor(uint256 tslaMintedValueUsd, uint256 collateralValueUsd)
+    function _calculateHealthFactor(uint256 xauMintedValueUsd, uint256 collateralValueUsd)
         internal
         pure
         returns (uint256)
     {
-        if (tslaMintedValueUsd == 0) return type(uint256).max;
+        if (xauMintedValueUsd == 0) return type(uint256).max;
         uint256 collateralAdjustedForThreshold = (collateralValueUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
-        return (collateralAdjustedForThreshold * PRECISION) / tslaMintedValueUsd;
+        return (collateralAdjustedForThreshold * PRECISION) / xauMintedValueUsd;
     }
 
     function _getAccountInformation(address user)
         private
         view
-        returns (uint256 totalTslaMinted, uint256 totalCollateralEth)
+        returns (uint256 totalXauMinted, uint256 totalCollateralEth)
     {
-        totalTslaMinted = s_tslaMintedPerUser[user];
+        totalXauMinted = s_xauMintedPerUser[user];
         totalCollateralEth = s_ethCollateralPerUser[user];
     }
 }
